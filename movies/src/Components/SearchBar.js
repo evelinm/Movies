1f35f329e9b1fe5e70194fde29de4dcc514fd35a
evelinm <mandamurillo910@gmail.com>
2021-04-28 14:38:38 -0700

    styled the search bar

--- movies/src/Components/SearchBar.js
+++ movies/src/Components/SearchBar.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import styled from 'styled-components';
 
 
 function SearchBar () {
+    const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
 
     return (
-        <div>this is the search bar</div>
+    <SearchBarContainer>
+   <form action="/" method="get">
+        <label htmlFor="header-search">
+        <span className="visually-hidden">Search Movie Poster</span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Search blog posts"
+            name="s" 
+        />
+        <button type="submit">Search</button>
+    </form>
+        </SearchBarContainer>
 
     )
 }
 export default SearchBar;
+
+
+const SearchBarContainer = styled.div`
+  display:flex;
+  align-items: center;
+  justify-content: center;
+
+  padding-top: 50px;
+  
+
+
+`
