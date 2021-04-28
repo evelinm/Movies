0a695a2034d70e38cacc9bf02ce8cdea97300f38
evelinm <mandamurillo910@gmail.com>
2021-04-28 14:18:46 -0700

    adding a search bar component

--- movies/src/Components/Body.js
+++ movies/src/Components/Body.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SearchBar from '../Components/SearchBar'
 
 function Body() {
    return (
        <div>
+       <SearchBar/>
            This is the body
+
        </div>
    ) 
 }
