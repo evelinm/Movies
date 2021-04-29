9d6e1b27a57f060916e4004b53749cfd09d73299
evelinm <mandamurillo910@gmail.com>
2021-04-28 17:24:48 -0700

    added the images

--- movies/src/Components/Body.js
+++ movies/src/Components/Body.js
@@ -2,26 +2,41 @@ import React, {useState,useEffect} from 'react';
 import SearchBar from '../Components/SearchBar';
 import Api from '../API/Api';
 
-const apiKey = "a26a19d8891d64b1fe4c5d91e5e5ffff";
-function Body() {
-    const [poster, setPoster] = useState([])
+import styled from 'styled-components';
 
-    useEffect(async () => {
-        const result = await Api.get('movie/upcoming?api_key=' + apiKey+ '&language=en-US&page=1');
-        console.log(result.data.results);
-        setPoster(result.data.results);
+const apiKey = "a26a19d8891d64b1fe4c5d91e5e5ffff";
+const imageUrl = "https://image.tmdb.org/t/p/w185/";
+
+function Body() {
+    const [poster, setPoster] = useState({})
+    const [isLoading, setIsLoading] =useState(true);
+
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const result = await Api.get('movie/upcoming?api_key=' + apiKey+ '&language=en-US&page=1');
+            setPoster(result.data);
+            setIsLoading(false)
+            console.log(result.data);
+          };
+        
+        fetchData();
       }, []);
    return (
        <div>
        <SearchBar/>
-        <ul>
-        {poster.map(item => (
-            <li key={item.id}><p>{item.title}</p></li>
+        {isLoading ?  <p> Loading...</p> : <ul>
+        {poster.results.map(item => (
+            <li key={item.id}>
+            <p>{item.title}</p>
+            <img src={`${imageUrl}${item.poster_path}`} alt="image"/>
+            </li>
             
         ))}
-        </ul>
+        </ul>}
 
-       </div>
+       </div> 
    ) 
 }
 export default Body;
